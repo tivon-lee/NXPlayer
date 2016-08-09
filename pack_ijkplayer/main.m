337e9a0d155f3d06ed55c1e1da9cb73ef2459c4b
tivon.lee <ltl@entstudy.com>
2016-08-09 17:51:20 +0800

    初始化

---
+++ pack_ijkplayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pack_ijkplayer
+//
+//  Created by leetvin on 8/9/16.
+//  Copyright © 2016 entstudy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
